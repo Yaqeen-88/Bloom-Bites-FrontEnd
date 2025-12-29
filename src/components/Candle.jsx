876b5a1baefa7435b69a1a95915422522d4d8759
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2025-12-29 12:44:56 +0300

    getCandles func and useEffect added

--- src/components/Candle.jsx
+++ src/components/Candle.jsx
@@ -9,6 +9,36 @@ const Candle = () => {
   const [actCandle, setActCandle] = useState(null)
   const [loading, setLoading] = useState(true)
 
+  const getCandles = async () => {
+    try {
+      setLoading(true)
+
+      const response = await Bloom.get("/candles")
+      const list = response.data
+      setCandles(list)
+
+      if (id) {
+        let here = null
+        for (let i=0; i < list.length; i++) {
+          if (list[i]._id === id) {
+            here = list[i]
+            break
+          }
+        }
+        setActCandle(here)
+      }
+
+      setLoading(false)
+    } catch (error) {
+      console.log("Error! Something happened getting candles", error);
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getCandles()
+  }, [id])
+
   return <></>
 }
 
